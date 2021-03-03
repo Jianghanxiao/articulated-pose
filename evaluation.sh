5721d34d54770554ab9e332c9938476e2ccf52ed
Jianghanxiao <shawn_jiang@sfu.ca>
2021-03-03 00:54:34 -0800

    latest

--- evaluation.sh
+++ evaluation.sh
@@ -3,13 +3,13 @@ python compute_gt_pose.py --item='drawer' --domain='unseen' --nocs='ANCSH' --sav
 python compute_gt_pose.py --item='drawer' --domain='unseen' --nocs='NAOCS' --save
 
 # run our processing over test group
-python pose_multi_process.py --item='drawer' --domain='unseen'
+python pose_multi_process.py --item='drawer' --domain='unseen' --nocs='ANCSH'
 
-# pose & relative joint rotation
-python eval_pose_err.py --item='drawer' --domain='unseen' --nocs='ANCSH'
+# # pose & relative joint rotation
+# python eval_pose_err.py --item='drawer' --domain='unseen' --nocs='ANCSH'
 
-# 3d miou estimation
-python compute_miou.py --item='eyeglasses' --domain='unseen' --nocs='ANCSH'
+# # 3d miou estimation
+# python compute_miou.py --item='eyeglasses' --domain='unseen' --nocs='ANCSH'
 
 # performance on joint estimations 
 python eval_joint_params.py --item='drawer' --domain='unseen' --nocs='ANCSH'
